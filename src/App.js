39e187ec9b2d4664c96d022e5c9c729271b9edbc
Rosengren <kev.rosengren@gmail.com>
2018-08-06 20:04:35 -0700

    Add search capability

--- src/App.js
+++ src/App.js
@@ -21,6 +21,13 @@ const list = [
   }
 ];
 
+function isSearched(searchTerm) {
+  return function (item) {
+    return item.title.toLowerCase()
+      .includes(searchTerm.toLowerCase());
+  }
+}
+
 class App extends Component {
 
   constructor(props) {
@@ -28,9 +35,15 @@ class App extends Component {
 
     this.state = {
       list,
+      searchTerm: '',
     };
 
+    this.onSearchChange = this.onSearchChange.bind(this);
     this.onDismiss = this.onDismiss.bind(this);
+  }
+
+  onSearchChange(event) {
+    this.setState({ searchTerm: event.target.value });
   }
 
   onDismiss(id) {
@@ -42,22 +55,36 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        {this.state.list.map(item =>
-          <div key={item.objectID}>
-            <span>
-              <a href={item.url}>{item.title}</a>
-            </span>
-            <span>{item.author}</span>
-            <span>{item.num_comments}</span>
-            <span>{item.points}</span>
-            <span>
-              <button
-                onClick={() => this.onDismiss(item.objectID)}
-                type="button">
-                Dismiss
-              </button>
-            </span>
-          </div>
+        <form>
+          <input
+            type="text"
+            onChange={this.onSearchChange}
+          />
+        </form>
+        {this.state.list.filter(isSearched(
+          this.state.searchTerm
+        )).map(item => {
+          const onHandleDismiss = () =>
+            this.onDismiss(item.objectID);
+
+          return (
+            <div key={item.objectID}>
+              <span>
+                <a href={item.url}>{item.title}</a>
+              </span>
+              <span>{item.author}</span>
+              <span>{item.num_comments}</span>
+              <span>{item.points}</span>
+              <span>
+                <button
+                  onClick={() => this.onDismiss(item.objectID)}
+                  type="button">
+                  Dismiss
+                </button>
+              </span>
+            </div>
+          );
+        }
         )}
       </div>
     );
